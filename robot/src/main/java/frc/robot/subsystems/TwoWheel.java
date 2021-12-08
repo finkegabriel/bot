951ef89c83c7ffb0f0b3477dc5661afdba6d312d
finkegabriel <finkegabriel@gmail.com>
2021-12-08 14:22:42 -0700

    init

---
+++ robot/src/main/java/frc/robot/subsystems/TwoWheel.java
@@ -0,0 +1,12 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class TwoWheel extends SubsystemBase {
+    /**
+     * Creates a new TwoWheel.
+     */
+    public TwoWheel() {
+        
+    }
+}
